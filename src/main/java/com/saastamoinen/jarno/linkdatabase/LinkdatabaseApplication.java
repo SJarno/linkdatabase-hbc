39a2a6049a47a06d3febda6182013785579eaf89
SJarno <jarno.saastamoinen@hotmail.com>
2021-11-09 14:59:22 +0200

    init

---
+++ src/main/java/com/saastamoinen/jarno/linkdatabase/LinkdatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.saastamoinen.jarno.linkdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkdatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkdatabaseApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/saastamoinen/jarno/linkdatabase/controllers/DefaultController.java
@@ -0,0 +1,14 @@
+package com.saastamoinen.jarno.linkdatabase.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DefaultController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+    
+}
