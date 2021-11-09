7c9055e572bcd199d1ffea0ea08296ffe3323aa0
SJarno <jarno.saastamoinen@hotmail.com>
2021-11-09 17:02:30 +0200

    add jpa repo

--- src/main/java/com/saastamoinen/jarno/linkdatabase/repositories/LinkRepository.java
+++ src/main/java/com/saastamoinen/jarno/linkdatabase/repositories/LinkRepository.java
@@ -2,8 +2,9 @@ package com.saastamoinen.jarno.linkdatabase.repositories;
 
 import com.saastamoinen.jarno.linkdatabase.models.Link;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LinkRepository extends CrudRepository<Link, Long>{
+
+public interface LinkRepository extends JpaRepository<Link, Long>{
     
 }
