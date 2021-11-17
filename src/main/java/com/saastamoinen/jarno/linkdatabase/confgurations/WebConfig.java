7b6516654b51aabf6ea8fe54d1de8a1e6fed2bab
SJarno <jarno.saastamoinen@hotmail.com>
2021-11-17 10:37:07 +0200

    production heroku

--- src/main/java/com/saastamoinen/jarno/linkdatabase/confgurations/WebConfig.java
+++ src/main/java/com/saastamoinen/jarno/linkdatabase/confgurations/WebConfig.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-@Profile("testing")
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
 
