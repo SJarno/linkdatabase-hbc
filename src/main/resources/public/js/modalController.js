69e23c6b03793b7dbc592d0802515a106dccbc69
SJarno <jarno.saastamoinen@hotmail.com>
2021-12-07 15:24:15 +0200

    rename close button id

--- src/main/resources/public/js/modalController.js
+++ src/main/resources/public/js/modalController.js
@@ -1,37 +1,11 @@
-/* var modal = document.getElementById("mainModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("modify-addlink-button");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-} */
 
 const toggleModal = (element) => {
   var modal = document.getElementById("mainModal-" + element.id);
   modal.style.display = "block";
   // Get the button that opens the modal
-  //var btn = document.getElementById("modalToggle-" + element.id);
-  console.log("JOtain tekee!")
   // Get the <span> element that closes the modal
   /* var span = document.getElementsByClassName("close")[0]; */
-  const span = document.getElementById("test-"+element.id);
+  const span = document.getElementById("closing-"+element.id);
 
   // When the user clicks on the button, open the modal
 
@@ -39,7 +13,6 @@ const toggleModal = (element) => {
   // When the user clicks on <span> (x), close the modal
   span.onclick = function () {
     modal.style.display = "none";
-    console.log("Pitäisi mennä kiinni")
   }
 
   // When the user clicks anywhere outside of the modal, close it
