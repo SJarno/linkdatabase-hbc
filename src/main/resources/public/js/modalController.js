7c84727d6ae34a433272cff311ba8f5ae50badf0
SJarno <jarno.saastamoinen@hotmail.com>
2021-12-07 19:06:09 +0200

    fix mobile view

--- src/main/resources/public/js/modalController.js
+++ src/main/resources/public/js/modalController.js
@@ -4,11 +4,7 @@ const toggleModal = (element) => {
   modal.style.display = "block";
   // Get the button that opens the modal
   // Get the <span> element that closes the modal
-  /* var span = document.getElementsByClassName("close")[0]; */
   const span = document.getElementById("closing-"+element.id);
-
-  // When the user clicks on the button, open the modal
-
 
   // When the user clicks on <span> (x), close the modal
   span.onclick = function () {
