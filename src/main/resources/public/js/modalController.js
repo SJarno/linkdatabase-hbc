1d80c8bdd9ac0014add28efe27de41007115b891
SJarno <jarno.saastamoinen@hotmail.com>
2021-12-07 15:21:29 +0200

    closing button bug fixed

--- src/main/resources/public/js/modalController.js
+++ src/main/resources/public/js/modalController.js
@@ -28,18 +28,18 @@ const toggleModal = (element) => {
   modal.style.display = "block";
   // Get the button that opens the modal
   //var btn = document.getElementById("modalToggle-" + element.id);
-
+  console.log("JOtain tekee!")
   // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
+  /* var span = document.getElementsByClassName("close")[0]; */
+  const span = document.getElementById("test-"+element.id);
 
   // When the user clicks on the button, open the modal
-
-  
 
 
   // When the user clicks on <span> (x), close the modal
   span.onclick = function () {
     modal.style.display = "none";
+    console.log("Pitäisi mennä kiinni")
   }
 
   // When the user clicks anywhere outside of the modal, close it
